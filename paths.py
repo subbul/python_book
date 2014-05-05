ec9ac5512d54803e61a110333406862930898d99
subbu <subbul@gmail.com>
2014-05-05 22:26:46 +0530

    additions

--- paths.py
+++ paths.py
@@ -16,3 +16,16 @@ print(os.path.basename("some/directory/file.jpg"))#returs only the file element
 print(os.path.dirname("some/directory/file.jpg"))#return directory name
 
 print(os.path.splittext("abc.jpeg")) #returns tuple containing filename and its extension
+
+print(os.name()) #returns 'nt' for windows based system
+
+print(os.path.exists("C:\Unknown")) #should return false
+print(os.path.exists("C:\windows")) #should return true
+print(os.path.isfile("C:\Windows\AppPatch\AcRes.dll")) #should retunr true as this file exists
+
+import glob
+glob.glob("*") # returns all files - wildcard
+glob.glob("*.py") #returns only .py files
+
+
+#os walk
